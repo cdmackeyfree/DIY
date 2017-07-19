30bddc55a838f860bff11ceed668c7c243918553
Crystal Free <YOUR@EMAIL.com>
2017-07-19 15:22:02 -0400

    sqlite sucks

--- app/assets/javascripts/calculator.js
+++ app/assets/javascripts/calculator.js
@@ -2,5 +2,5 @@ function calculateBy()
 {
         length = document.getElementById("length").value;
         width = document.getElementById("width").value;
-        document.getElementById("result").innerHTML = (length * width) /64;
+        document.getElementById("result").floor = (length * width) /64;
 }
