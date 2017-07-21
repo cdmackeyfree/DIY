7f9590ee509b3ac19cbacf209ea8627c287f450a
Crystal Free <YOUR@EMAIL.com>
2017-07-21 15:21:39 -0400

    hover text working right

--- app/assets/javascripts/texthover.js
+++ app/assets/javascripts/texthover.js
@@ -1,17 +1,7 @@
-$(document).ready( function() {
-	$('.description').hide();
-});
-
-$(document).ready( function() {
+$(document).on('ready page:load', function() {
   $('.imagecontainer').hover( function() {
     $(this).find('.description').fadeIn(300);
     }, function() {
     $(this).find('.description').fadeOut(100);
     });
-});
-
-$(document).ready( function () {
-	$('.imagecontainer').click( function(){
-		$(this).find('.imageborder').border('color', 'red');
-	});
 });
